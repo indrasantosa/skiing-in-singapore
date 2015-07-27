f050312f888715966e7f5cb68e019645052bbc8a
Indra Santosa <indrasantosa90@gmail.com>
2015-07-27 14:06:32 +0800

    Add lanscape wiring

--- index.js
+++ index.js
@@ -27,21 +27,31 @@ Skimap.prototype.traceMap = function (filePath) {
 
 	var mapText = fs.readFileSync(filePath, { encoding: 'utf8' });
 
+	// Temporary variable before validated
+	var mapWidth = 0;
+	var mapHeight = 0;
 	var mapArray = [];
+
+	// Process map extraction
 	var mapInfoArray = mapText.split('\n');
 
 	var extractHeader = function(headerString) {
 		var headerArray = headerString.trim().split(' ');
-		self.mapWidth = parseInt(headerArray[0]);
-		self.mapHeight = parseInt(headerArray[1]);
+		mapWidth = parseInt(headerArray[0]);
+		mapHeight = parseInt(headerArray[1]);
 	}
 	var extractRow = function(rowString) {
 		var itemArray = rowString.trim().split(' ');
+
+		// Iterate through array to push landscape data to array
 		for(var i = 0; i < itemArray.length; i++) {
-			var newItem = {
-				elevation: parseInt(itemArray[i])
-			};
-			mapArray.push(newItem);
+			var content = parseInt(itemArray[i]);
+			if(!isNaN(content)) {
+				var newItem = {
+					elevation: content
+				};
+				mapArray.push(newItem);
+			}
 		}
 	}
 
@@ -50,7 +60,34 @@ Skimap.prototype.traceMap = function (filePath) {
 		extractRow(mapInfoArray[i]);
 	}
 
-	this.map = mapArray;
+	var wireupLandscape = function() {
+		// Wireup the map
+		for (var i = 1; i <= self.mapWidth; i++) {
+			for (var j = 1; j <= self.mapHeight; j++) {
+				var current = self.getLandscape(i, j);
+				current.top = self.getLandscape(i, j-1);
+				current.right = self.getLandscape(i+1, j);
+				current.bottom = self.getLandscape(i, j+1);
+				current.left = self.getLandscape(i-1, j);
+			}
+		}
+	}
+
+	// Validate the result of the extraction
+	if(mapArray.length === mapWidth*mapHeight) {
+		this.map = mapArray;
+		this.mapWidth = mapWidth;
+		this.mapHeight = mapHeight;
+		wireupLandscape();
+		return true;
+	} else {
+		return false;
+	}
+
+};
+
+Skimap.prototype.getLandscape = function (pointX, pointY) {
+	return this.map[((pointY-1)*this.mapWidth) + (pointX-1)] || undefined;
 };
 
 if(module) {
--- test/test.js
+++ test/test.js
@@ -25,6 +25,51 @@ describe('Unit Test', function() {
 
 		});
 
+		describe('.getLandscape', function() {
+
+			it('Should return correct landscape', function() {
+				var skimap = new Skimap({
+					filePath: __dirname + '/files/test.txt'
+				});
+				skimap.getLandscape(1, 2).should.not.be.equal(undefined);
+				skimap.getLandscape(1, 2).elevation.should.be.equal(2);
+
+				skimap.getLandscape(4, 3).should.not.be.equal(undefined);
+				skimap.getLandscape(4, 3).elevation.should.be.equal(5);
+
+				skimap.getLandscape(4, 4).should.not.be.equal(undefined);
+				skimap.getLandscape(4, 4).elevation.should.be.equal(6);
+
+				(typeof skimap.getLandscape(5, 5)).should.be.equal('undefined');
+			});
+
+		});
+
+		describe('.traceMap', function() {
+
+			it('Should return true if successful', function() {
+				var skimap = new Skimap();
+				skimap.traceMap(__dirname + '/files/test.txt')
+					.should.be.equal(true);
+			});
+			it('Should return false if unsuccessful', function() {
+				var skimap = new Skimap();
+				skimap.traceMap(__dirname + '/files/test-fail.txt')
+					.should.be.equal(false);
+			});
+			it('Should wire the landscape currectly', function() {
+				var skimap = new Skimap({
+					filePath: __dirname + '/files/test.txt'
+				});
+				skimap.getLandscape(2, 2).right.elevation.should.be.equal(9);
+				skimap.getLandscape(2, 4).top.elevation.should.be.equal(3);
+				skimap.getLandscape(3, 2).bottom.elevation.should.be.equal(2);
+				skimap.getLandscape(2, 4).left.elevation.should.be.equal(4);
+				(typeof skimap.getLandscape(2, 4).bottom).should.be.equal('undefined');
+			});
+
+		});
+
 	});
 
 });
